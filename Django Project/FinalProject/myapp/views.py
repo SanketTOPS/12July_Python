63924f900b655acc90a9761a2fbbfd76a4962f39
SanketTops <sanket.tops@gmail.com>
2023-12-11 15:07:41 +0530

    Login-Signup

--- Django Project/FinalProject/myapp/views.py
+++ Django Project/FinalProject/myapp/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from .forms import signupForm
+from .models import usersignup
 
 # Create your views here.
 def index(request):
+    if request.method=='POST': #root
+        if request.POST.get('signup')=='signup': #signup
+            newuser=signupForm(request.POST)
+            if newuser.is_valid():
+                newuser.save()
+                print("Signup Successfully!")
+            else:
+                print(newuser.errors)
+
+        elif request.POST.get('login')=='login': #login
+
+            unm=request.POST['username']
+            pas=request.POST['password']
+
+            user=usersignup.objects.filter(username=unm,password=pas)
+            if user:
+                print("Login successfully!")
+            else:
+                print("Error!Login fail.....Try again")
     return render(request,'index.html')
 
 def notes(request):
