d599975d27c23a5162aef3edc1dadd9f965e2a25
SanketTops <sanket.tops@gmail.com>
2024-01-01 14:59:20 +0530

    OTP-Jazzmin

--- Django Project/FinalProject/myapp/views.py
+++ Django Project/FinalProject/myapp/views.py
@@ -4,6 +4,8 @@ from .models import *
 from django.contrib.auth import logout
 from django.core.mail import send_mail
 from FinalProject import settings
+import random
+import requests
 
 # Create your views here.
 def index(request):
@@ -74,13 +76,23 @@ def contact(request):
             print("Feedback send successfully!")
 
             #Email Sending
+            otp=random.randint(1111,9999)
             sub="Thank You!"
-            msg=f"Dear User!\n\nThanks for your feedback, we will connect shortly!\n\nIf any queries regarding, you can contact us\n\n+91 9724799469 | help@tops-int.com\n\nThanks & Regards!\nTOPS Tech - Rajkot\nwww.tops-int.com"
+            msg=f"Dear User!\n\nThanks for your feedback, we will connect shortly!\n\nIf any queries regarding, you can contact us\n\n Your one time password is f{otp}\n\n+91 9724799469 | help@tops-int.com\n\nThanks & Regards!\nTOPS Tech - Rajkot\nwww.tops-int.com"
             from_email=settings.EMAIL_HOST_USER
             #to_email=['kotechamit5@gmail.com','parthhirpara89827@gmail.com','krishnakachhad20@gmail.com','yogitabeladiya2425@gmail.com','rinkalbhad245@gmail.com','janvivora244@gmail.com','vrutikadudhat3@gmail.com','tahjudin597@gmail.com']
             to_email=[request.POST['email']]
-
             send_mail(subject=sub,message=msg,from_email=from_email,recipient_list=to_email)
+
+            #SMS-OTP Sending
+            url = "https://www.fast2sms.com/dev/bulkV2"
+            #querystring = {"authorization":"Your_API","variables_values":f"{otp}","route":"otp","numbers":f"{request.POST['mob']}"}
+            querystring = {"authorization":"Your_API","message":f"Dear User\nThis msg from TOPS Technologies, Your SS session has been scheduled.Plz visit www.tops-int.com","language":"english","route":"q","numbers":f"{request.POST['mob']}"}
+            headers = {
+                'cache-control': "no-cache"
+            }
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            print(response.text)
 
         else:
             print(new.errors)
