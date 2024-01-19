2e2c883086a9595c15ba6037cb0987d9d53e312c
SanketTops <sanket.tops@gmail.com>
2024-01-19 14:19:24 +0530

    Final Notes

--- Django Project/FinalProject/FinalProject/settings.py
+++ Django Project/FinalProject/FinalProject/settings.py
@@ -111,21 +111,26 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+#LOCAL SERVER 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
